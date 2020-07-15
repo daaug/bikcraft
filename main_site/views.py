7ce264fe6a92bebbef036edc65ff5c8625da0514
Daniel Augusto <daniel.aug.souza@gmail.com>
2020-07-15 02:16:48 -0300

    Testing home response

--- main_site/views.py
+++ main_site/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = { "name": "daneil", }
+    return render(request, "main_site/index.html", context)
